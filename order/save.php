e26aab6fd1fe1b45b5bf7a257c76379e5669d10a
joobasb <aromaajoonas@gmail.com>
2022-12-09 14:29:59 +0200

    ADD: order function

---
+++ order/save.php
@@ -0,0 +1,58 @@
+<?php
+
+require_once '../inc/functions.php';
+require_once '../inc/headers.php';
+
+$db = null;
+
+$input = json_decode(file_get_contents('php://input'));
+$fname = filter_var($input->firstname, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$lname = filter_var($input->lastname, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$address = filter_var($input->address, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$zip = filter_var($input->zip, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$city = filter_var($input->city, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$email = filter_var($input->email, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$cart = $input->cart;
+
+try {
+    $db = openDb();
+    $db->beginTransaction();
+
+    //lisää asiakas
+    $sql = "INSERT INTO customer (firstname,lastname,address,zip,city,email) VALUES 
+    ('" .
+        filter_var($fname,FILTER_SANITIZE_FULL_SPECIAL_CHARS) . "','" .
+        filter_var($lname,FILTER_SANITIZE_FULL_SPECIAL_CHARS) . "','" .
+        filter_var($address,FILTER_SANITIZE_FULL_SPECIAL_CHARS) . "','" .
+        filter_var($zip,FILTER_SANITIZE_FULL_SPECIAL_CHARS) . "','" .
+        filter_var($city,FILTER_SANITIZE_FULL_SPECIAL_CHARS) . "','" .
+        filter_var($email, FILTER_SANITIZE_FULL_SPECIAL_CHARS)
+        . "')";
+
+    $customer_id = executeInsert($db,$sql);
+
+    //lisää tilaus
+    $sql = "INSERT INTO `order` (customer_id) VALUES ($customer_id)";
+    $order_id = executeInsert($db,$sql);
+
+    //lisää tilausrivit
+    foreach ($cart as $product) {
+    $sql = "INSERT INTO order_row (order_id,product_id, amount) VALUES ("
+    .
+        $order_id . "," .
+        $product->id . "," . 
+        $product->amount 
+    .   ")";
+    executeInsert($db,$sql);
+    }
+    $db->commit();
+    header('HTTP/1.1 200 OK');
+    $data = array('id' => $customer_id);
+    echo json_encode($data);
+}
+catch (PDOException $pdoex) {
+    $db->rollback();
+    returnError($pdoex);
+    echo($pdoex);
+}
+?>
